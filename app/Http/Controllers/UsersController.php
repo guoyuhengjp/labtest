d56fd951f1a5807b5945b6b64c8d99cb6041aeb6
guoyuhengjp <guoyuhengjp@gmail.com>
2019-02-15 10:13:29 +0000

    画像の処理

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -49,7 +49,7 @@ class UsersController extends Controller
 
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id,416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'LaraBBS') Laravelで掲示板</title>
+  <title>@yield('title', 'LaraBBS') Laravel掲示板</title>
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
