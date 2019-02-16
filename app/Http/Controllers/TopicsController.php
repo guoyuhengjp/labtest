2357c36e6e47b13f7fa56feded368c870963d282
guoyuhengjp <guoyuhengjp@gmail.com>
2019-02-16 04:52:23 +0000

    翻訳URL

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -40,8 +40,13 @@ class TopicsController extends Controller
      * @author kaku
      * @createtime 2019.02.15
      */
-    public function show(Topic $topic)
+    public function show(Request $request, Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -71,7 +76,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '投稿成功しました');
+        return redirect()->to($topic->link())->with('success', '投稿成功しました！');
     }
 
     /**
